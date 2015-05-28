adcf96a3d1c4ee9e23242f05f2556ad4999e91a8
Jan Priessnitz <honya121@gmail.com>
2015-05-28 15:39:13 +0200

    Minor fix

--- power_indication.py
+++ power_indication.py
@@ -11,59 +11,57 @@ class PowerIndication:
 			GPIO.setup(18, GPIO.OUT)				#inicializace pinu pro eth0_link_led
 			GPIO.output(17, GPIO.HIGH)				#rozsviceni power_led, zhasne, az se RasPi vypne
 			GPIO.output(18, GPIO.LOW)
-			self.flog = file("log.out", "w");
-			self.frx = io.open("/sys/class/net/eth0/statistics/rx_bytes", "r");
-			self.ftx = io.open("/sys/class/net/eth0/statistics/tx_bytes", "r");
-			self.rx = 0;
-			self.tx = 0;
-			self.oldrx = 0;
-			self.oldtx = 0;
-			self.eth_led_state = True;
-			self.power_button_hold = False;
+			self.flog = file("log.out", "w")
+			self.frx = io.open("/sys/class/net/eth0/statistics/rx_bytes", "r")
+			self.ftx = io.open("/sys/class/net/eth0/statistics/tx_bytes", "r")
+			self.rx = 0
+			self.tx = 0
+			self.oldrx = 0
+			self.oldtx = 0
+			self.eth_led_state = True
+			self.power_button_hold = False
 	def loop(self):
-			if GPIO.input(4) == GPIO.LOW:
-				return True
-			self.oldrx = rx;
-			self.oldtx = tx;
-			self.frx.seek(0);
-			self.ftx.seek(0);
-			self.rx = int(frx.read().strip());
-			self.tx = int(ftx.read().strip());
-			os.system("ethtool eth0 | grep 'Link detected:' > tmp.out");
-			output = open("tmp.out", "r").read().strip();
+			self.oldrx = rx
+			self.oldtx = tx
+			self.frx.seek(0)
+			self.ftx.seek(0)
+			self.rx = int(frx.read().strip())
+			self.tx = int(ftx.read().strip())
+			os.system("ethtool eth0 | grep 'Link detected:' > tmp.out")
+			output = open("tmp.out", "r").read().strip()
 			if(output == "Link detected: yes"):
-				GPIO.output(18, GPIO.HIGH);
-				self.flog.write(output);
+				GPIO.output(18, GPIO.HIGH)
+				self.flog.write(output)
 				if(self.rx != self.oldrx or self.tx != self.oldtx):
 					if(self.eth_led_state):
-						GPIO.output(18, GPIO.HIGH);
-						self.eth_led_state = False;
+						GPIO.output(18, GPIO.HIGH)
+						self.eth_led_state = False
 					else:
-						GPIO.output(18, GPIO.LOW);
-						self.eth_led_state = True;
+						GPIO.output(18, GPIO.LOW)
+						self.eth_led_state = True
 			else:
-				GPIO.output(18, GPIO.OUT);
+				GPIO.output(18, GPIO.OUT)
 			if GPIO.input(4) != GPIO.HIGH:
-				self.power_button_hold = True;
+				self.power_button_hold = True
 			time.sleep(0.125)
 			if GPIO.input(4) != GPIO.HIGH and self.power_button_hold == True:
-				break;
-			self.power_button_hold = False;
+				return True
+			self.power_button_hold = False
+			return False
 	def powerOff(self):
-		GPIO.output(17, GPIO.LOW);
-		time.sleep(0.25);
-		GPIO.output(17, GPIO.HIGH);
-		time.sleep(0.25);
-		GPIO.output(17, GPIO.LOW);
-		time.sleep(0.25);
-		GPIO.output(17, GPIO.HIGH);
-		time.sleep(0.25);
-		GPIO.output(17, GPIO.LOW);
-		time.sleep(0.25);
-		GPIO.output(17, GPIO.HIGH);
-		time.sleep(0.25);
-		GPIO.output(17, GPIO.LOW);
-		time.sleep(0.25);
-		GPIO.output(17, GPIO.HIGH);
+		GPIO.output(17, GPIO.LOW)
+		time.sleep(0.25)
+		GPIO.output(17, GPIO.HIGH)
+		time.sleep(0.25)
+		GPIO.output(17, GPIO.LOW)
+		time.sleep(0.25)
+		GPIO.output(17, GPIO.HIGH)
+		time.sleep(0.25)
+		GPIO.output(17, GPIO.LOW)
+		time.sleep(0.25)
+		GPIO.output(17, GPIO.HIGH)
+		time.sleep(0.25)
+		GPIO.output(17, GPIO.LOW)
+		time.sleep(0.25)
+		GPIO.output(17, GPIO.HIGH)
 		os.system("poweroff")					#vypnuti RasPi
-		
